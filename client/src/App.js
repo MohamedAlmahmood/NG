5a762e02fc15d52a8b6869ab596998d9a7071bc0
MohamedAlmahmood <xhamadeex@gmail.com>
2021-02-08 11:39:56 +0300

    Boss tasks working

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,67 @@
 import './App.css';
+import {useState} from "react";
+import Axios from 'axios';
 import { 
   Input, 
   Button
 } from 'semantic-ui-react'
 
 function App() {
+  const [BossName, setBossName] = useState("");
+  const [EmployeeName, setEmployeeName] =useState("");
+  const [BossInput, setBossInput] = useState("");
+  const [EmployeeInput, setEmployeeInput] = useState("");
+  const [BossInputsList, setBossInputsList] = useState([]);
+
+  const addBossTask = ()=>{
+    Axios.post('http://localhost:3001/addBossTask', {
+      BossName: BossName,
+      BossInput: BossInput,
+    }).then( ()=>{
+      setBossInputsList([...BossInputsList, {
+        bossName: BossName,
+        bossTask: BossInput,
+      }])
+    })
+  }
+
+  const addEmployeeTask = ()=>{
+    Axios.post('http://localhost:3001/addEmployeeTask', {
+      EmployeeName: EmployeeName,
+      EmployeeInput: EmployeeInput,
+    })
+  }
+
+  const showBossTask = ()=>{
+    Axios.get('http://localhost:3001/showBossTasks').then((response)=>{
+      setBossInputsList(response.data);
+    })
+  }
+
+
   return (
     <div className="App">
       <div className="AssigningTask">
-        <Input fluid icon='' placeholder='Assign Task...' />
-        <Button content='Submit' primary />
-        <Button content='Show All' secondary />
+        <Input fluid icon='' placeholder='Your Name' onChange={(event)=> {setBossName(event.target.value)}}/>
+        <Input fluid icon='' placeholder='Assign Task...' onChange={(event)=> {setBossInput(event.target.value)}}/>
+        <Button content='Submit' primary onClick={addBossTask}/>
+        <Button content='Show All' secondary onClick={showBossTask} />
+        {BossInputsList.map((val,key)=>{
+          return(
+            <div className="showBossTasks">
+              <h1>By: {val.bossName}</h1>
+              <p1>{val.bossTask}</p1>
+            </div>
+          )
+        })}
       </div>
 
       <hr/>
 
       <div className="responseTask">
-        <Input fluid icon='' placeholder='What tasks did you complete today?' />
-        <Button content='Submit' primary />
+        <Input fluid icon='' placeholder='Your Name' onChange={(event)=> {setEmployeeName(event.target.value)}}/>
+        <Input fluid icon='' placeholder='What tasks did you complete today?' onChange={ (event)=> {setEmployeeInput(event.target.value) }}/>
+        <Button content='Submit' primary onClick={addEmployeeTask}/>
         <Button content='Show All' secondary />
       </div>
 
---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {extended: false} ))
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'password',
+    database: 'tasks',
+});
+
+app.listen(3001, ()=>{
+    console.log("server running on port 3001")
+})
+
+app.post('/addBossTask', (req, res)=>{
+    const BossName = req.body.BossName;
+    const BossInput = req.body.BossInput;
+    db.query(
+        "INSERT INTO tasks.bosstable (bossName, bossTask) VALUES (?,?)", 
+        [BossName, BossInput],
+        (err, result)=>{
+            if (err){
+                console.log(err)
+            }else{
+                res.send("values inserted")}
+        }
+    )
+})
+
+app.get('/showBossTasks', (req,res)=>{
+    db.query(
+        "SELECT * FROM tasks.bosstable", (err, result)=>{
+            if (err){
+                console.log(err)
+            }else {
+                res.send(result);
+            }
+        }
+    )
+})
